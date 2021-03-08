d34f3ffa2895f87900eb11fa857393cb89ebd725
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2021-03-08 23:59:49 +0100

    a better create call

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -2,14 +2,29 @@
 
 use codec::{Decode, Encode};
 use frame_support::{
-    decl_error, decl_event, decl_module, decl_storage, traits::Randomness, RuntimeDebug,
-    StorageDoubleMap, StorageValue,
+    decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult,
+    traits::Randomness, RuntimeDebug, StorageDoubleMap, StorageValue,
 }; // StorageValue and StorageDoubleMap is needed if we want to access the get method in our module
 use frame_system::ensure_signed;
 use sp_io::hashing::blake2_128;
 
 #[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, Eq)]
+pub enum KittyGender {
+    Male,
+    Female,
+}
+#[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, Eq)]
 pub struct Kitty(pub [u8; 16]); // Our type Kitty
+
+impl Kitty {
+    pub fn gender(&self) -> KittyGender {
+        if self.0[0] % 2 == 0 {
+            KittyGender::Male
+        } else {
+            KittyGender::Female
+        }
+    }
+}
 
 pub trait Trait: frame_system::Trait {
     type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
@@ -49,24 +64,27 @@ decl_module! {
         pub fn create(origin) {
             let sender = ensure_signed(origin)?;
 
-           Self::next_kitty_id().checked_add(1).ok_or(Error::<T>::KittiesIdOverflow)?;
+            NextKittyId::try_mutate(|next_id| -> DispatchResult {
+                let current_id = *next_id;
+                *next_id = next_id.checked_add(1).ok_or(Error::<T>::KittiesIdOverflow)?;
 
-            // Generate a random 128bit value
-            let payload = (
-                <pallet_randomness_collective_flip::Module<T> as Randomness<T::Hash>>::random_seed(),
-                &sender,
-                <frame_system::Module<T>>::extrinsic_index(),
-            );
-            let dna = payload.using_encoded(blake2_128);
+                // Generate a random 128bit value
+                let payload = (
+                    <pallet_randomness_collective_flip::Module<T> as Randomness<T::Hash>>::random_seed(),
+                    &sender,
+                    <frame_system::Module<T>>::extrinsic_index(),
+                );
+                let dna = payload.using_encoded(blake2_128);
 
-            // Create and store kitty
-            let kitty = Kitty(dna);
-            let kitty_id = Self::next_kitty_id();
-            Kitties::<T>::insert(&sender, kitty_id, kitty.clone());
-            NextKittyId::put(kitty_id + 1);
+                // Create and store kitty
+                let kitty = Kitty(dna);
+                Kitties::<T>::insert(&sender, current_id, kitty.clone());
 
-            // Emit event
-            Self::deposit_event(RawEvent::KittyCreated(sender, kitty_id, kitty))
+                // Emit event
+                Self::deposit_event(RawEvent::KittyCreated(sender, current_id, kitty));
+
+                Ok(())
+            })?;
         }
 
     }
